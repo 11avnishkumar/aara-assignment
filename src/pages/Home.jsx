9d18807d176742e7ae7e6e3a6c889c9d802dfbad
avnish kumar <11avnishkumar@gmail.com>
2023-06-25 22:15:37 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
     <div className="mt-20 max-w-xl mx-auto">
       <div className="flex flex-col justify-center items-center  p-4 rounded-md h-[80vh]">
         <div className="w-full">
-          <h1 className="mb-4 text-4xl font-semibold text-gray-400">
+          <h1 className="mb-4 text-3xl md:text-4xl font-semibold text-gray-400">
             {!isSubmitted ? "Enter Phone Number" : "One Time Password"}
           </h1>
           <form className="w-full">
