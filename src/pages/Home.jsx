0d4c52d342b9d304d18868dfd2ebfbd41b6d4e78
avnish kumar <11avnishkumar@gmail.com>
2023-06-25 20:03:03 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-
 import VerificationInput from "../components/VerificationInput";
 import PhoneNumberInput from "../components/PhoneNumberInput";
+import { useGLobalContext } from "../context/ProfileContext";
 const Home = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [otp, setOtp] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
-
+  const { getToken } = useGLobalContext();
+  if (getToken) {
+    return (
+      <div className="mt-20 max-w-xl mx-auto">
+        <div className="flex flex-col justify-center items-center  p-4 rounded-md h-[80vh]">
+          <h1 className="text-2xl md:text-5xl mb-2">Welcome User</h1>
+          <p className="text-center">Your are on home page </p>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="mt-20 max-w-xl mx-auto">
       <div className="flex flex-col justify-center items-center  p-4 rounded-md h-[80vh]">
